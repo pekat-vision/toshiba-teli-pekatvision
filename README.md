5db1c3a39e5170c0e1f8a894595f77a199e728e2
Adam Svidron <adam.svidron@pekatvision.com>
2020-11-17 23:02:16 +0100

    Adding C++ example

--- README.md
+++ README.md
@@ -1,13 +1,46 @@
-## TeliCam SDK Python Wrapper + PEKAT VISION
+# Toshiba Teli & Pekat example
 
-### Usage
-```shell
-export TELICAMSDK=/opt/TeliCamSDK
-export LD_LIBRARY_PATH=$TELICAMSDK/lib:$TELICAMSDK/genicam/bin/Linux64_x64:$LD_LIBRARY_PATH
-python3 ./pekat_teli_sample.py
+The example shows how to connect to Toshiba Teli camera, acquire one image and analyze it in Pekat. Both Python and C++ examples require Toshiba Teli
+SDK to be installed on your system. You will also need to modify the code to reference your instance of Pekat server - just search for `host` in
+Python and `pv_create_remote_analyzer` in C++.
+
+## Python
+
+[Pekat Vision SDK for Python](https://github.com/pekat-vision/pekat-vision-sdk-python) is required either installed or you can include it in
+`PYTHONPATH` enviroment variable. You also need to put Toshiba Teli libraries path into LD_LIBRARY_PATH variable to correctly find dependencies:
+
+```sh
+TELICAMSDK=/opt/TeliCamSDK
+export LD_LIBRARY_PATH="$TELICAMSDK/lib:$TELICAMSDK/genicam/bin/Linux64_x64:$LD_LIBRARY_PATH"
+python3 pekat_teli_sample.py
+```
+On Windows you do not need to set paths to Toshiba Teli SDK libraries, just run the sample.
+
+Note that attached `teli.py` module provides access to part of TeliCamSDK. It does not provide access to all the features or the SDK but only subset
+needed to set camera parameters and acquire image using simple API.
+
+## C++
+
+[Pekat Vision SDK for ANSI C](https://github.com/pekat-vision/pekat-vision-sdk-ansi-c) is required for C++ example.
+
+To compile and run the example on Linux you also need libCURL at least 7.62.0 with development support installed (`curl-config` must be in your
+path).
+
+```sh
+# Path to Pekat SDK
+PEKATSDK=pekat-vision-sdk-ansi-c
+# Path to Toshiba Teli SDK
+TELICAMSDK=/opt/TeliCamSDK
+gcc "-I$TELICAMSDK/include" "-L$TELICAMSDK/lib" -lTeliCamApi -lTeliCamUtl `curl-config --cflags` `curl-config --libs` "-I$PEKATSDK" "$PEKATSDK/pekatvision/sdk_linux.c" pekat_teli_sample.cpp -o pekat_teli_sample
 ```
 
-### Prerequisites
-- Python 3.6+
-- opencv-python
-- https://github.com/pekat-vision/pekat-vision-sdk-python
+To run the sample, you need to set LD_LIBRARY_PATH as for Python:
+
+```sh
+export LD_LIBRARY_PATH="$TELICAMSDK/lib:$TELICAMSDK/genicam/bin/Linux64_x64:$LD_LIBRARY_PATH"
+./pekat_teli_sample
+```
+
+On Windows, use attached Visual Studio project. You will need `TELICAMSDK` enviroment variable set (done by Toshiba Teli SDK installer) and also
+Pekat Vision SDK in directory named `PekatVisionSDK` one level above the sample project (i.e. `..\PekatVisionSDK`). Or you can change the reference
+to project and include directory. Then you can just compile and run the example.
---
+++ pekat_teli_sample.cpp
@@ -0,0 +1,177 @@
+#ifdef _WIN32
+    // Don't warn about fopen_s
+    #define _CRT_SECURE_NO_WARNINGS
+    #include <windows.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <TeliCamApi.h>
+#include <TeliCamUtl.h>
+#include <pekatvision/sdk.h>
+
+using namespace Teli;
+
+int main() {
+    CAM_API_STATUS status;
+    CAM_HANDLE hCam = (CAM_HANDLE)NULL;
+    SIGNAL_HANDLE hSignal = (SIGNAL_HANDLE)NULL;
+    CAM_STRM_HANDLE hStream = (CAM_STRM_HANDLE)NULL;
+    void *buffer = NULL;
+    void *image = NULL;
+    pv_analyzer *pekat = NULL;
+    uint32_t cameras;
+
+    status = Sys_Initialize();
+    if (status) {
+        printf("Sys_Initialize error: 0x%08x\n", status);
+        return 1;
+    }
+
+    do {
+        status = Sys_GetNumOfCameras(&cameras);
+        if (status) {
+            printf("Sys_GetNumOfCameras error: 0x%08x\n", status);
+            break;
+        }
+        printf("Number of cameras: %d\n", cameras);
+
+        for (int i = 0; i < cameras; i++) {
+            CAM_INFO info;
+
+            status = Cam_GetInformation((CAM_HANDLE)NULL, i, &info);
+            if (status) {
+                printf("Cam_GetInformation error: 0x%08x\n", status);
+                break;
+            }
+
+            printf("Camera %d\n", i);
+            printf("  Camera type: %s\n", info.eCamType == CAM_TYPE_U3V ? "USB3" : info.eCamType == CAM_TYPE_GEV ? "GigE" : "unknown");
+            printf("  Manufacturer: %s\n", info.szManufacturer);
+            printf("  Model: %s\n", info.szModelName);
+            printf("  Serial no: %s\n", info.szSerialNumber);
+            printf("  User name: %s\n", info.szUserDefinedName);
+        }
+
+        if (cameras) {
+            uint32_t maxBufSize;
+
+            // Create Pekat analyzer
+            pekat = pv_create_remote_analyzer("51.124.161.74", 8100, NULL);
+            if (!pekat) {
+                printf("Failed to create Pekat analyzer\n");
+                break;
+            }
+            // Open camera
+            status = Cam_Open(0, &hCam);
+            if (status) {
+                printf("Cam_Open error: 0x%08x\n", status);
+                break;
+            }
+
+            // Here you could set various properties using SDK
+
+            // Create signal
+            status = Sys_CreateSignal(&hSignal);
+            if (status) {
+                printf("Sys_CreateSignal error: 0x%08x\n", status);
+                break;
+            }
+            // Open stream
+            status = Strm_OpenSimple(hCam, &hStream, &maxBufSize, hSignal);
+            if (status) {
+                printf("Strm_OpenSimple error: 0x%08x\n", status);
+                break;
+            }
+            // Prepare buffer
+            buffer = malloc(maxBufSize);
+            if (!buffer) {
+                printf("Cannot allocate %d bytes of memory\n", maxBufSize);
+                break;
+            }
+            // Start streaming
+            status = Strm_Start(hStream);
+            if (status) {
+                printf("Strm_Start error: 0x%08x\n", status);
+                break;
+            }
+            // Wait for image
+            status = Sys_WaitForSignal(hSignal, 2000);
+            if (status == CAM_API_STS_SUCCESS) {
+                // Image ready
+                CAM_IMAGE_INFO imageInfo;
+                uint32_t bufSize = maxBufSize;
+
+                // Get image
+                status = Strm_ReadCurrentImage(hStream, buffer, &bufSize, &imageInfo);
+                if (status) {
+                    printf("Strm_ReadCurrentImage error: 0x%08x\n", status);
+                    break;
+                }
+                // Allocate buffer for RGB image
+                image = malloc(imageInfo.uiSizeX * imageInfo.uiSizeY * 3);
+                if (!image) {
+                    printf("Cannot allocate memory for %dx%d image\n", imageInfo.uiSizeX, imageInfo.uiSizeY);
+                    break;
+                }
+                // Convert to RGB
+                status = ConvImage(DST_FMT_BGR24, imageInfo.uiPixelFormat, true, image, buffer, imageInfo.uiSizeX, imageInfo.uiSizeY);
+                if (status) {
+                    printf("ConvImage error: 0x%08x\n", status);
+                    break;
+                }
+                // Save as bitmap
+                status = SaveBmpRGB(image, imageInfo.uiSizeX, imageInfo.uiSizeY, "pict-orig.bmp");
+                if (status) {
+                    printf("SaveBmpRGB error: 0x%08x\n", status);
+                    break;
+                }
+                // Analyze by Pekat
+                int res = pv_analyze_raw_image(pekat, (char*)image, imageInfo.uiSizeX, imageInfo.uiSizeY, PVRT_ANNOTATED_IMAGE, NULL);
+                if (res) {
+                    printf("Analysis in Pekat failed: %d\n", res);
+                    break;
+                } else {
+                    char *res_image = pv_get_result_data(pekat);
+                    int res_size = pv_get_result_data_size(pekat);
+                    FILE *f = fopen("pict-annotated.png", "wb");
+                    if (f) {
+                        fwrite(res_image, res_size, 1, f);
+                        fclose(f);
+                    } else {
+                        printf("Cannot save Pekat result image to disk\n");
+                    }
+                }
+            } else if (status == CAM_API_STS_TIMEOUT) {
+                // Not received
+                printf("Image not received in time\n");
+            } else {
+                printf("Sys_WaitForSignal error: 0x%08x\n", status);
+                break;
+            }
+        }
+    } while (0);
+
+    // Cleanup
+    if (hStream) {
+        Strm_Stop(hStream);
+        Strm_Close(hStream);
+    }
+    if (hSignal) {
+        Sys_CloseSignal(hSignal);
+    }
+    if (hCam) {
+        Cam_Close(hCam);
+    }
+    if (image) {
+        free(image);
+    }
+    if (buffer) {
+        free(buffer);
+    }
+    if (pekat) {
+        pv_free_analyzer(pekat);
+    }
+    Sys_Terminate();
+    return status ? 1 : 0;
+}
